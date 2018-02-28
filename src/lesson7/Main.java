7120a892829230d23d1c6f196ac71c1d50691879
AlexandrBerdar <officialofberdar@gmail.com>
2018-02-28 21:01:38 +0200

    new lesson

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -4,26 +4,42 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        int[] array = new int[1000];
-        Arrays.fill(array,5);
-        Counter[] counters = new Counter[10];
-        for (int i = 0; i < 10 ; i++) {
-            counters[i] = new Counter(array,i*100,(i+1)*100);
-            counters[i].start();
+        int[] array = new int[10000];
+        Arrays.fill(array, 5);
+        for (int i = 1; i < 10; i++) {
+            double time = calculate(i,array);
+            System.out.println(i + ": " + time);
+
 
         }
-        for (Counter counter : counters){
-            counter.join();
-        }
-        long result = 0;
-        for (Counter counter : counters){
-            result += counter.getResult();
-        }
-        System.out.println(result);
     }
+        static double calculate(int threads, int[] array) throws InterruptedException{
+            long time = System.currentTimeMillis();
+            Counter[] counters = new Counter[threads];
+            double attempts = 1000;
+            for (int attempt = 0; attempt < attempts; attempt++) {
+                for (int i = 0; i < threads; i++) {
+                    counters[i] = new Counter(array, i * 100, (i + 1) * 100);
+                    counters[i].start();
+
+                }
+                for (Counter counter : counters) {
+                    counter.join();
+                }
+                long result = 0;
+                for (Counter counter : counters) {
+                    result += counter.getResult();
+                }
+            }
+            time = System.currentTimeMillis() - time;
+            return time / attempts;
+
+        }
 
 
-static class Counter extends Thread {
+
+
+    static class Counter extends Thread {
     private int[] array;
     private int start, stop;
     private long result;
